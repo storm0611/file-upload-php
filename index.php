678625d22dcd904541fba47e016f201892055c97
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-10-11 02:35:16 -0700

    complete

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1> File Upload Functionality</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <hr>
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+</body>
+
+</html>
