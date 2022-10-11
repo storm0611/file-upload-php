64caa67634ac2c33ebd0659e1272b012269294b6
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-10-11 02:42:49 -0700

    done

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <h1> File Upload Functionality</h1>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         <p>Select image to upload:</p>
-        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="file" name="fileToUpload" id="fileToUpload" style="border: 1px solid grey; border-radius: 5px;">
         <hr>
         <input type="submit" value="Upload Image" name="submit">
     </form>
